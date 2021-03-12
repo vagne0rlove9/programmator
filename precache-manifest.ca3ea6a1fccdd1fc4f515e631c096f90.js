2a1f969dbe44de23102875488edd396b1d3eb474
vagne0rlove9 <kirik-pos@mail.ru>
2021-03-12 07:49:50 +0300

    Updates

---
+++ precache-manifest.ca3ea6a1fccdd1fc4f515e631c096f90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c157c8930ac7305e85392e5cac223a7",
+    "url": "/programmator/index.html"
+  },
+  {
+    "revision": "972ee30c7b656b4667ce",
+    "url": "/programmator/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "690c949f25786e4d2f29",
+    "url": "/programmator/static/css/main.69a5d1e8.chunk.css"
+  },
+  {
+    "revision": "972ee30c7b656b4667ce",
+    "url": "/programmator/static/js/2.07d483cc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/programmator/static/js/2.07d483cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690c949f25786e4d2f29",
+    "url": "/programmator/static/js/main.661c0d93.chunk.js"
+  },
+  {
+    "revision": "9ba18c45585567c9a717",
+    "url": "/programmator/static/js/runtime-main.3f8ddb15.js"
+  }
+]);
